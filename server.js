14a0419cdfdfc0f527cd19f92cabbe6294691ebb
vaungsophal <2023451vaung@aupp.edu.kh>
2026-05-21 17:01:03 +0700

    update the port

--- server.js
+++ server.js
@@ -71,6 +71,6 @@ app.get('/', (req, res) => {
   res.send('NodeJS Express Server is running!');
 });
 
-app.listen(PORT, '127.0.0.1', () => {
-  console.log(`Server running on http://localhost:${PORT}`);
+app.listen(PORT, '0.0.0.0', () => {
+  console.log(`Server running on http://0.0.0.0:${PORT}`);
 });
